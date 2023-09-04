9969ddfcaac0759f8284d331f6a3a666f0da5e1a
Thomas Robinson <git@tomr.me>
2023-09-04 16:08:20 +0100

    map attributions

--- map.js
+++ map.js
@@ -8,9 +8,14 @@ let bounds = L.latLngBounds();
 let groups = [];
 let markers = [];
 
+const extraAtts = ` |
+made by <a href="https://tomr.me">tom</a> &bull;
+<a href="https://github.com/itsmeimtom/station-mapper">about &amp; source</a>
+`;
+
 // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // 	maxZoom: 19,
 // 	attribution: '© OpenStreetMap'
 // }).addTo(map);
 
-L.tileLayer('https://basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png', { attribution: `Tiles by <a href="https://carto.com/">Carto</a>` }).addTo(map);
+L.tileLayer('https://basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png', { attribution: `tiles by <a href="https://carto.com/">Carto</a>${extraAtts}` }).addTo(map);;
